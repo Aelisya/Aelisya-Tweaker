9a62534342751d7f38fdbd2eea24415d419daa7c
Aelisya <42673166+aelisya@users.noreply.github.com>
2020-10-16 02:05:10 +0200

    Security and bugproofing
    Add some security and protect curent page if background js don't work or bug

--- js/background.js
+++ js/background.js
@@ -1,4 +1,16 @@
-document.querySelectorAll("[target='_blank']").forEach(e=>
-{
-    e.setAttribute("rel","noopener noreferrer")
-});
+try {
+    const amp = document.head.querySelector("link[rel~='amphtml'][href]");
+    if (amp.href !== document.location.href){
+        document.location.replace(amp.href);
+    }
+} catch (e) {
+    try {
+        const canonical = document.head.querySelector("link[rel~='canonical'][href]")
+        if (canonical.href !== document.location.href){
+            document.location.replace(canonical.href);
+        }
+    } catch (e) {}
+}
+try {
+    document.querySelectorAll("[target='_blank']").forEach(e=>{e.setAttribute("rel","noopener noreferrer nofollow")});
+} catch (e) {}
--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,5 @@
 document.getElementById('appName').textContent = chrome.i18n.getMessage('appName');
-let version = document.getElementById('version');
+const version = document.getElementById('version');
 version.textContent += chrome.i18n.getMessage('version');
 version.textContent += " " + chrome.runtime.getManifest().version;
 document.getElementById('dev').textContent = chrome.i18n.getMessage('about');
