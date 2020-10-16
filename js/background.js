480acbc8bab6cebaa83589453d4a007935cdce87
Aelisya <42673166+aelisya@users.noreply.github.com>
2020-10-16 23:58:47 +0200

    Optimisation
    Optimisation of AMP redirector in background.js

--- js/background.js
+++ js/background.js
@@ -1,16 +1,10 @@
 try {
-    const amp = document.head.querySelector("link[rel~='amphtml'][href]");
+    const amp = document.head.querySelector("link[rel~='amphtml'][href]") || document.head.querySelector("link[rel~='canonical'][href]");
     if (amp.href !== document.location.href) {
         document.location.replace(amp.href);
     }
-} catch (e) {
-    try {
-        const canonical = document.head.querySelector("link[rel~='canonical'][href]")
-        if (canonical.href !== document.location.href) {
-            document.location.replace(canonical.href);
-        }
-    } catch (e) {}
-}
+} catch (e) {}
+
 try {
     document.querySelectorAll("[target='_blank']").forEach(e => {
         e.setAttribute("rel","noopener noreferrer nofollow")
