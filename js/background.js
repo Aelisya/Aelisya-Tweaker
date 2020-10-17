0a34c6011e864cc222072f0ef8c587fca7a1110d
Aelisya <42673166+aelisya@users.noreply.github.com>
2020-10-17 18:31:12 +0200

    Removing old code rewrited
    remove code who redirect link who don't have any utility on desktop and code a major bug on amp content enabled page.

--- js/background.js
+++ js/background.js
@@ -1,11 +1,4 @@
 try {
-    const amp = document.head.querySelector("link[rel~='amphtml'][href]") || document.head.querySelector("link[rel~='canonical'][href]");
-    if (amp.href !== document.location.href) {
-        document.location.replace(amp.href);
-    }
-} catch (e) {}
-
-try {
     document.querySelectorAll("[target='_blank']").forEach(e => {
         e.setAttribute("rel","noopener noreferrer nofollow")
     });
