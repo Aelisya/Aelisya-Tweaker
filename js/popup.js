c7ff40cc34e0fd8a6b75f8ea1e1206a18398ed0a
aelisya <42673166+aelisya@users.noreply.github.com>
2020-10-23 00:47:33 +0200

    Add bug report
    add bug report link

--- js/popup.js
+++ js/popup.js
@@ -1,11 +1,11 @@
 const i18n = chrome.i18n;
 const manifest = chrome.runtime.getManifest();
-const appname = document.getElementById('appName');
 const version = document.getElementById('version');
 const privacy = document.getElementById('privacy');
 const licence = document.getElementById('licence');
+const feature = document.getElementById('feature');
 
 version.textContent = i18n.getMessage('version') + " " + manifest.version;
 privacy.textContent = i18n.getMessage('privacy');
 licence.textContent = i18n.getMessage('licence');
-appname.textContent = manifest.name;
+feature.textContent = i18n.getMessage('feature');
