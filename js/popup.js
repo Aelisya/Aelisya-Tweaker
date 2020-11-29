ac73931e529badca5565fa45bc56891e6275b793
Michael B <bevode@aelisya.net>
2020-11-29 19:16:35 +0100

    Update popup.js

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,2 @@
 document.getElementById('version').textContent=chrome.i18n.getMessage('version')+" "+chrome.runtime.getManifest().version;
 document.getElementById('feature').textContent=chrome.i18n.getMessage('feature');
-const options=document.getElementById('options');
-options.textContent=chrome.i18n.getMessage('options');
-options.addEventListener('click',()=>{chrome.runtime.openOptionsPage()});
